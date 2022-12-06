2d8d97fc82812cff4eeff3bb25d12389b992e2aa
Anoja Paraniroopasingam <ap3620@ic.ac.uk>
2022-12-06 17:12:59 +0000

    fin

---
+++ src/main/java/org/example/ButtonPanel.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonPanel extends JPanel {
+    JButton but1 = new JButton("BUTTON");
+    //but1=new JButton("Press me");
+    public ButtonPanel(){
+        but1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hello");
+
+            }
+
+        });
+        add(but1);
+    }
+
+
+}
---
+++ src/main/java/org/example/SImpleUI.java
@@ -0,0 +1,25 @@
+package org.example;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class SImpleUI {
+
+    static GraphicsConfiguration gc;	// Class field containing config info
+    public static void main(String[] args) {
+        JFrame frame= new JFrame(gc);	// Create a new JFrame
+        frame.setSize(500,300);
+        frame.getContentPane().add(new ButtonPanel());
+        frame.setVisible(true);
+        // This next line closes the program when the frame is closed
+        frame.addWindowListener(new WindowAdapter() {	// Closes the program if close window clicked
+            public void windowClosing(WindowEvent e) {
+                frame.dispose();
+            }
+        });
+
+    }}
+
